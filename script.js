fcd4f67f0d6f35839a150e26ff5327490d78f644
suzchi <galacticsuz@gmail.com>
2022-10-13 06:57:30 -0600

    Add playRound functionality

--- script.js
+++ script.js
@@ -1,40 +1,42 @@
 let playerSelection = window.prompt("Choose your weapon:"); // Prompts user to choose between "rock", "paper", and "scissors"
 playerSelection = playerSelection.toLowerCase(); { // Changes user input to all lowercase
     if (playerSelection === "rock") {
-        playerSelection = 0;
-    } else if (playerSelection === "paper") {
         playerSelection = 1;
-    } else if (playerSelection === "scissors") {
+    } else if (playerSelection === "paper") {
         playerSelection = 2;
+    } else if (playerSelection === "scissors") {
+        playerSelection = 3;
+    } else if (playerSelection != "rock", "paper", "scissors") {
+        playerSelection = null;
     }
 }
-console.log(playerSelection, "player");
+console.log(playerSelection, "player selection (1 = rock, 2 = paper, 3 = scissors");
 
 
-
-let randomInteger = Math.floor(Math.random() * 3) // Get 0, 1, 2
-
-let compChoice;
-
+let randomInteger = Math.floor(Math.random() * 3 + 1) // Get 0, 1, 2
 // vv Takes randomInteger and turns it into computer's choice vv
-function getComputerChoice() { 
-    if (randomInteger === 0) {
-        compChoice = 0
-        console.log(compChoice, "computer");
-        return compChoice;
-    } else if (randomInteger === 1) {
-        compChoice = 1
-        console.log(compChoice, "computer");
-        return compChoice;
-    } else if (randomInteger === 2) {
-        compChoice = 2
-        console.log(compChoice, "computer");
-        return compChoice;
+let computerSelection = randomInteger;
+console.log(computerSelection, "computer selection (1 = rock, 2 = paper, 3 = scissors");
+
+let roundResult;
+// 1 = rock, 2 = paper, 3 = scissors
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        roundResult = "You Tied!";
+        return roundResult;
+    } else if (playerSelection === 1 && computerSelection === 3) {
+        roundResult = "You won! Rock beats scissors!";
+        return roundResult;
+    } else if (playerSelection === 2 && computerSelection === 1) {
+        roundResult = "You won! Paper beats rock!";
+        return roundResult;
+    } else if (playerSelection === 3 && computerSelection === 2) {
+        roundResult = "You won! Scissors beats paper"
+        return roundResult;
     } else {
-        compChoice = null
-        console.log(compChoice, "computer");
-        return compChoice;
+        roundResult = "You lose!"
+        return roundResult;
     }
 }
-
-const computerSelection = getComputerChoice();
+console.log(roundResult);
+console.log(playRound(playerSelection, computerSelection)); // Calls playRound
